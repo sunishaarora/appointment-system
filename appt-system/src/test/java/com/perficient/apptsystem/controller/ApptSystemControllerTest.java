a61755b4053f895b9ba8d89d7da856f4be2e3e9a
aashi-agr <aashi.agrawal@perficient.com>
2023-07-18 15:50:16 -0500

    Ready for demo!

--- appt-system/src/test/java/com/perficient/apptsystem/controller/ApptSystemControllerTest.java
+++ appt-system/src/test/java/com/perficient/apptsystem/controller/ApptSystemControllerTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.ParameterizedTypeReference;
@@ -13,6 +16,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -23,40 +28,37 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApptSystemControllerTest {
 
     @MockBean
     private RestTemplate restTemplate;
 
-    @InjectMocks
+    @Autowired
+    private MockMvc mockMvc;
     private ApptSystemController controller;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        controller = new ApptSystemController(restTemplate);
+        controller = new ApptSystemController(restTemplate, "http://mock-users", "http://mock-appts");
     }
     @Test
-    void deleteUser_Success() {
-        Long userId = 1L;
-        ResponseEntity<Void> responseEntity = ResponseEntity.ok().build();
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.DELETE), any(), eq(Void.class), eq(userId))).thenReturn(responseEntity);
-        ResponseEntity<?> response = controller.deleteUser(userId);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        verify(restTemplate, times(1)).exchange(anyString(), eq(HttpMethod.DELETE), any(), eq(Void.class), eq(userId));
+    void getAllUsers_Success() throws Exception {
+        List<User> users = Arrays.asList(
+                new User(1L, "John", "Doe", "Male", 25, "john.doe@gmail.com", "3144411316"),
+                new User(2L, "Jane", "Wakes", "Female", 26, "jane.wakes@gmail.com", "3144411317")
+        );
+        ResponseEntity<List<User>> responseEntity = ResponseEntity.ok(users);
+        when(restTemplate.exchange(any(), HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
+        })).thenReturn(responseEntity);
     }
-        @Test
-        void getAllUsers_Success() throws Exception{
-            List<User> users =Arrays.asList(
-                    new User(1L, "John", "Doe", "Male", 25, "john.doe@gmail.com", "3144411316"),
-                    new User(2L, "Jane", "Wakes", "Female", 26, "jane.wakes@gmail.com", "3144411317")
-            );
-            ResponseEntity<List<User>> responseEntity = ResponseEntity.ok(users);
-            when(restTemplate.exchange("http://localhost:8100/api/v1/users", HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
-            })).thenReturn(responseEntity);
-        }
+
     @Test
     void getUserByName_Success() throws Exception {
         String firstName = "John";
@@ -65,13 +67,14 @@ class ApptSystemControllerTest {
         uriVariables.put("firstName", firstName);
         uriVariables.put("lastName", lastName);
 
-        List<User> userList = Arrays.asList(new User());
+        List<User> userList = List.of(new User());
 
         when(restTemplate.exchange(
                 eq("http://localhost:8100/api/v1/user/search/{firstName}/{lastName}"),
                 eq(HttpMethod.GET),
                 isNull(),
-                eq(new ParameterizedTypeReference<List<User>>() {}),
+                eq(new ParameterizedTypeReference<List<User>>() {
+                }),
                 eq(uriVariables)
         )).thenReturn(ResponseEntity.ok(userList));
 
@@ -86,21 +89,24 @@ class ApptSystemControllerTest {
                 eq("http://localhost:8100/api/v1/user/search/{firstName}/{lastName}"),
                 eq(HttpMethod.GET),
                 isNull(),
-                eq(new ParameterizedTypeReference<List<User>>() {}),
+                eq(new ParameterizedTypeReference<List<User>>() {
+                }),
                 eq(uriVariables)
         );
     }
-        @Test
-        void updateUser_Success() throws Exception{
-            String url = "http://localhost:8100/api/v1/updateUser/{userId}";
-            User user = new User();
-            ResponseEntity<User> response = ResponseEntity.ok(user);
-            when(restTemplate.exchange(eq(url), eq(HttpMethod.PUT), any(HttpEntity.class), eq(User.class), eq(1L)))
-                    .thenReturn(response);
-            ResponseEntity<?> responseEntity = controller.updateUser(1L, new User(1L, "John", "Doe", "Male", 25, "john.doe@gmail.com", "3144411316"));
-            assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-            assertEquals(user, responseEntity.getBody());
+
+    @Test
+    void updateUser_Success() throws Exception {
+        String url = "http://localhost:8100/api/v1/updateUser/{userId}";
+        User user = new User();
+        ResponseEntity<User> response = ResponseEntity.ok(user);
+        when(restTemplate.exchange(eq(url), eq(HttpMethod.PUT), any(HttpEntity.class), eq(User.class), eq(1L)))
+                .thenReturn(response);
+        ResponseEntity<?> responseEntity = controller.updateUser(1L, new User(1L, "John", "Doe", "Male", 25, "john.doe@gmail.com", "3144411316"));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(user, responseEntity.getBody());
     }
+
     @Test
     public void createUser_Success() {
         User user = new User();
@@ -114,6 +120,7 @@ class ApptSystemControllerTest {
         verify(restTemplate, times(1)).postForObject("http://localhost:8100/api/v1/users", user, User.class);
 
     }
+
     @Test
     public void testCreateAppt() {
         Appts appt = new Appts();
@@ -158,7 +165,7 @@ class ApptSystemControllerTest {
     }
 
     @Test
-    public void testGetAppt(){
+    public void testGetAppt() {
         Long id = 1L;
         Appts appt = new Appts();
         when(restTemplate.getForEntity(eq("http://localhost:8200/api/v1/appts/{id}"), eq(Appts.class), eq(id)))
@@ -174,11 +181,12 @@ class ApptSystemControllerTest {
     }
 
     @Test
-    public void testGetApptList(){
+    public void testGetApptList() {
         List<Appts> apptList = Arrays.asList(new Appts(), new Appts());
 
         when(restTemplate.exchange(eq("http://localhost:8200/api/v1/appts"), eq(HttpMethod.GET), isNull(),
-                eq(new ParameterizedTypeReference<List<Appts>>() {})))
+                eq(new ParameterizedTypeReference<List<Appts>>() {
+                })))
                 .thenReturn(ResponseEntity.ok(apptList));
 
         ResponseEntity<List<Appts>> responseEntity = controller.getAllAppts();
@@ -188,7 +196,20 @@ class ApptSystemControllerTest {
         assertSame(apptList, responseEntity.getBody());
         verify(restTemplate, times(1))
                 .exchange(eq("http://localhost:8200/api/v1/appts"), eq(HttpMethod.GET), isNull(),
-                        eq(new ParameterizedTypeReference<List<Appts>>() {}));
+                        eq(new ParameterizedTypeReference<List<Appts>>() {
+                        }));
     }
 
 }
+
+
+//    @Test
+//    void deleteUser_Success() throws Exception {
+//        Long userId = 1L;
+//        ResponseEntity<Void> responseEntity = ResponseEntity.ok().build();
+//        when(restTemplate.exchange(anyString(), eq(HttpMethod.DELETE), any(), eq(Void.class), eq(userId))).thenReturn(responseEntity);
+////        ResponseEntity<?> response = controller.deleteUser(userId);
+////        assertEquals(HttpStatus.OK, response.getStatusCode());
+//        mockMvc.perform(delete("/management-system/deleteUser/{userId}", userId)).andExpect(status().isOk());
+//        verify(restTemplate, times(1)).exchange(anyString(), eq(HttpMethod.DELETE), any(), eq(Void.class), eq(userId));
+//    }
